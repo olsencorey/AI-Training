4245c264aa8b4273c35c0f88c747038c597b69c2
Corey Olsen <44654147+olsencorey@users.noreply.github.com>
2025-11-11 18:19:10 -0500

    Create index.jsx

---
+++ React/ReusableFooter/index.jsx
@@ -0,0 +1,31 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="#">🏠 Home</a>
+        </li>
+        <li>
+          <a href="#">💼 Portfolio</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">🌐 Blog</a>
+        </li>
+        <li>
+          <a href="#">📩 Contact</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">🔗 Link 1</a>
+        </li>
+        <li>
+          <a href="#">🔗 Link 2</a>
+        </li>
+      </ul>
+      <p>© 2025 Corey Olsen. All rights reserved.</p>
+    </footer>
+  );
+};
