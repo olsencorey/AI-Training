d51af6a75eef738767137aac524cb3fa3b45082c
Corey Olsen <44654147+olsencorey@users.noreply.github.com>
2025-12-12 19:44:55 -0500

    Create fibonacci-number-calculator.py

---
+++ Python/fibonacci-number-calculator.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n < 2:
+        return n
+    
+    sequence = [0, 1]
+
+    for i in range(2, n + 1):
+        sequence.append(sequence[i - 1] + sequence[i - 2])
+    
+    return sequence[n]
+
+print(fibonacci(15))
